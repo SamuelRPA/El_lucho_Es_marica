49dc6cade4a3384d2ba4cc3a502b095a987dee91
SamuelRPA <pas0034109@est.univalle.edu>
2023-05-10 14:23:11 -0400

    V2 El lucho es marica con suma

--- Program.cs
+++ Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("El lucho es marica");
+
+            int a, s;
+            a = 50;
+            s = 5645;
+            Console.WriteLine(a+s); 
         }
     }
 }
